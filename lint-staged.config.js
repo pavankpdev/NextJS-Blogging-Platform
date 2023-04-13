459bcac9bbe4601ef35c405c36c76076e31e244c
pavanleton <pavankumarkpkp@gmail.com>
2023-04-13 21:49:00 +0530

    beautify code

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -1,7 +1,8 @@
 module.exports = {
-  "**/*.(ts|tsx)": () => "pnpm tsc --noEmit",
-  "**/*.(ts|tsx|js)": (filenames) => {
-    `pnpm eslint --fix ${filenames.join(" ")}`
-    `pnpm prettier --write ${filenames.join(" ")}`
+  '**/*.(ts|tsx)': () => 'pnpm tsc --noEmit',
+  '**/*.(ts|tsx|js)': (filenames) => {
+    ;`pnpm eslint --fix ${filenames.join(
+      ' '
+    )}``pnpm prettier --write ${filenames.join(' ')}`
   },
 }
