576bc0fb69ba69dd2b273115ee4e00f3c3b9392f
pavanleton <pavankumarkpkp@gmail.com>
2023-04-17 21:27:21 +0530

    beautify code

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,11 +1,14 @@
-import { withClerkMiddleware } from "@clerk/nextjs/server";
-import { NextResponse } from "next/server";
+import { withClerkMiddleware } from '@clerk/nextjs/server'
+import { NextResponse } from 'next/server'
 import type { NextRequest } from 'next/server'
 
 export default withClerkMiddleware((req: NextRequest) => {
+  console.log(req)
   // run your logic here
-  return NextResponse.next();
-});
+  return NextResponse.next()
+})
 
 // Stop Middleware running on static files
-export const config = { matcher:  '/((?!_next/image|_next/static|favicon.ico).*)'};
+export const config = {
+  matcher: '/((?!_next/image|_next/static|favicon.ico).*)',
+}
