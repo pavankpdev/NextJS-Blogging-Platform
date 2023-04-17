3b265824ae7d5b6ad21bf2d3eef39a159003d0c4
pavanleton <pavankumarkpkp@gmail.com>
2023-04-17 21:35:22 +0530

    update (auth): add SignIn and SignUp

---
+++ src/pages/sign-up/[[...index]].tsx
@@ -0,0 +1,9 @@
+import { SignUp } from "@clerk/nextjs";
+
+const SignInPage = () => {
+  return <>
+    <SignUp path="/sign-up" routing="path" signInUrl={'/sign-in'} />
+  </>;
+}
+
+export default SignInPage
