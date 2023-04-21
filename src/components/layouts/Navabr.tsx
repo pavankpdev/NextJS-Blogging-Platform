3c27ce92969d3788f4908ef3ab64c3a6d0063b2d
pavanleton <pavankumarkpkp@gmail.com>
2023-04-21 20:16:47 +0530

    lint: linting and ignore next.config

---
+++ src/components/layouts/Navabr.tsx
@@ -0,0 +1,23 @@
+import {useAuth, UserButton, SignInButton} from "@clerk/nextjs";
+
+// STYLES
+import styles from '@/styles/Navbar.module.scss'
+const Navbar = () => {
+
+  const {isSignedIn} = useAuth()
+
+  return <>
+      <nav className={styles.navbar__container}>
+          <div className={styles.navbar__logo}>
+            <h1>Logo</h1>
+          </div>
+          <div className={styles.navbar__cta}>
+            {
+              isSignedIn ? <UserButton /> : <SignInButton />
+            }
+          </div>
+      </nav>
+  </>
+}
+
+export default Navbar
