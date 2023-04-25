1760e9ce1f7dff1248d397d08065e2327b9c0012
pavanleton <pavankumarkpkp@gmail.com>
2023-04-25 21:47:48 +0530

    config: axios

---
+++ src/config/axios.ts
@@ -0,0 +1,8 @@
+import axios from "axios";
+
+export const axiosInstance = axios.create({
+  baseURL: 'https://api.jsonbin.io/v3',
+  headers: {
+    'X-Access-Key': '$2b$10$sL29Fsjo9oo4rn0ixhhXnepkPiuQTlJtX/ZOiJ.zs8wY0Ez3GgV4S'
+  },
+})
