a9320d33e70683e3060593e9ae401e89ab738e64
pavanleton <pavankumarkpkp@gmail.com>
2023-05-01 21:14:35 +0530

    update: add basic editor layout styles

--- src/pages/blog/create.tsx
+++ src/pages/blog/create.tsx
@@ -1,4 +1,12 @@
 import dynamic from 'next/dynamic'
+import Head from "next/head";
+
+// COMPONENTS
+import Navbar from "@/components/layouts/Navabr";
+
+// STYLES
+import UtilityStyles from "@/styles/Utility.module.scss";
+import Styles from '@/styles/Editor.module.scss'
 
 const TipTap = dynamic(() => import('@/components/Editor/tiptap'), {
   ssr: false,
@@ -7,7 +15,18 @@ const TipTap = dynamic(() => import('@/components/Editor/tiptap'), {
 const Create = () => {
   return (
     <>
-      <TipTap />
+      <Head>
+        <title>Home</title>
+        <meta name="description" content="Home page" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <Navbar />
+      <main className={UtilityStyles.container}>
+        <div className={Styles.tiptap__container}>
+          <TipTap />
+        </div>
+      </main>
     </>
   )
 }
